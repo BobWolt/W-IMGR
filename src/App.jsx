c79015844259fab5b243635f563b3c817a9774ae
BobWolt <b.b.o.wolters@gmail.com>
2023-08-11 12:49:53 +0200

    modal test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import { useState } from 'react';
 import './App.css';
+import Modal from './components/modal';
 
 function App() {
 	return (
 		<>
 			<div className='flex flex-row gap-32'>
-				<h1 className='text-3xl text-blue-800 font-bold underline'>
-					Hello world!
-				</h1>
-				<h1 className='text-3xl text-red-800 font-bold underline'>
-					Tailwind CSS should be working and is injected into a single JS file!
-				</h1>
+				<Modal />
 			</div>
 		</>
 	);
---
+++ src/components/modal.jsx
@@ -0,0 +1,16 @@
+export default function Modal(props) {
+	return (
+		<div className='relative max-w-sm bg-white rounded overflow-hidden shadow-lg'>
+			<div className='h-full min-h-[300px] flex flex-col flex-wrap justify-between px-6 py-4'>
+				<div>
+					<div className='w-full flex justify-between'>
+						<p className='text-gray font-normal'>Joehoe</p>
+						<p className='text-gray font-normal'>ayoo</p>
+					</div>
+					<h3 className='font-bold text-4xl mb-2 mt-2'>??</h3>
+					<p className='text-grey-darker text-base'>jfnsdngs</p>
+				</div>
+			</div>
+		</div>
+	);
+}
