8ec35191f0100aaeaab176a7b640858954687601
BobWolt <b.b.o.wolters@gmail.com>
2023-08-11 12:20:35 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,14 @@ import './App.css';
 function App() {
 	return (
 		<>
-			<h1 className='text-3xl text-blue-800 font-bold underline'>
-				Hello world!
-			</h1>
+			<div className='flex flex-row gap-32'>
+				<h1 className='text-3xl text-blue-800 font-bold underline'>
+					Hello world!
+				</h1>
+				<h1 className='text-3xl text-red-800 font-bold underline'>
+					Tailwind CSS should be working and is injected into a single JS file!
+				</h1>
+			</div>
 		</>
 	);
 }
