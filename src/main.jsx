ee2370eb395f4643ec4a6c2f2297d763aa8d3acb
BobWolt <b.b.o.wolters@gmail.com>
2023-08-15 18:16:12 +0200

    container test

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './App.css';
 
-ReactDOM.createRoot(document.querySelector('body')).render(
+const widgetRoot = document.querySelectorAll('.wimgr-widget');
+console.log(widgetRoot);
+
+//const rootElement = document.querySelector('body');
+const rootElement = document.getElementById('w-imgr-container');
+const root = ReactDOM.createRoot(rootElement);
+
+/* ReactDOM.createRoot(document.querySelector('body')).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+); */
+
+root.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
 );
+
+/* widgetRoot.forEach((div) => {
+	ReactDOM.render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	);
+});
+ */
